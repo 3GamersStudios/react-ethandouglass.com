6ebbecddf0f8ed90c6f057edf78cdf4786a5dcd0
Ethan <thewotbros03@gmail.com>
2021-11-10 18:57:16 -0800

    Updated site to work with react router!

---
+++ src/2021_TRI1_Classwork.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class COMPSCI21_TRI1_Classwork extends Component{
+    render() {
+        return (
+            <div>Workin' Hard!</div>
+
+        );
+    }
+}
+
+export default COMPSCI21_TRI1_Classwork;
---
+++ src/2021_TRI1_T_talks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class COMPSCI21_TRI1_T_Talks extends Component {
+    render() {
+        return (
+            <div>T-Talkin'</div>
+
+        );
+    }
+}
+
+export default COMPSCI21_TRI1_T_Talks;
